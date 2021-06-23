c2ca565749fab3ea2761118bc8d99e11488c06ce
unknown <wilsonkaroki@gmail.com>
2021-06-23 14:22:59 +0300

    Code Era

---
+++ app.js
@@ -0,0 +1,10 @@
+document.querySelector(".ham-btn").
+addEventListener("click",() =>{
+    document.querySelector(".container").
+    classList.toggle("change");
+}); 
+
+//var age = prompt("whats your age");
+//console.log(age);
+
+//var deleteConfirm = confirm("Are you sure about this");
